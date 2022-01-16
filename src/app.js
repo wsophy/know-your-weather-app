e5d370fd0cde31115f61d40cbb0678a13dce5d13
wsophy <96415885+wsophy@users.noreply.github.com>
2022-01-16 20:10:59 +0800

    Showing the current tempeture

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
-let apiKey = "18b30d8f2f5b2ccbe094edcd7e5304da";
-let city = "h1";
-let apiUrl = `"https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}"`;
+function displayTemperature(response){
+    console.log(response.data.wind.speed);
+    let temperatureElement = document.querySelector ("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector ("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector ("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector ("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector ("#wind");
+    windElement.innerHTML = response.data.wind.speed;
+}
 
-console.log(city);
+
+let endpointUrl = "https://api.openweathermap.org/data/2.5/weather?";
+let tempetureUnits = "metric";
+let apiKey ="220e37eed186a27e272ac52b9c9a702c"; 
+let weatherUrl = `${endpointUrl}&q=New York&appid=${apiKey}&units=${tempetureUnits}`;
+
+axios.get(weatherUrl).then(displayTemperature);
+
+let now = new date();
