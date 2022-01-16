e944dfa97aa3ec2c3d0d0e5db23e0db680e6c9f2
wsophy <96415885+wsophy@users.noreply.github.com>
2022-01-17 00:27:45 +0800

    Implemented the unit convector

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ function displayTemperature(response){
     let dateElement = document. querySelector ("#date");
     let iconElement = document. querySelector ("#icon");
     
+    celsiusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -41,24 +43,45 @@ let weatherUrl = `${endpointUrl}&q=${city}&appid=${apiKey}&units=${tempetureUnit
 axios.get(weatherUrl).then(displayTemperature);
 }
 
-
-
 function handleSubmit(event){
 event.preventDefault();
 let cityInputElement = document.querySelector ("#city-input");
 search(cityInputElement.value);
+}
 
+function showFahrenheitTempeture (event) {
+event.preventDefault();
+let temperatureElement = document.querySelector ("#temperature");
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTempeture = (celsiusTemperature * 9) / 5 + 32;
+
+temperatureElement.innerHTML = Math.round(fahrenheitTempeture);
 }
 
 
+function showCelsiusTempeture (event) {
+event.preventDefault();
+let temperatureElement = document.querySelector ("#temperature");
+let celsiusTempeture = celsiusTemperature;
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+temperatureElement.innerHTML = Math.round(celsiusTempeture);
+}
 
 
-
-
+let celsiusTemperature = null;
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+
+let fahrenheitLink = document.querySelector ("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTempeture);
+
+let celsiusLink = document.querySelector ("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTempeture);
 
 
 search("New York");
