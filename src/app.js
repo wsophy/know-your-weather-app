fda4bfdeb11a941017f522b8ee6956a66a6c7337
wsophy <96415885+wsophy@users.noreply.github.com>
2022-01-16 20:54:26 +0800

    Formatted timestamp
    Setup  timestamp and date

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if(hours<10){
+    hours= `0${hours}`;
+}
+if(minutes<10){
+    minutes= `0${minutes}`;
+}
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day = days[date.getDay()];
+return`${day} ${hours}:${minutes}`;
+
+}
+
+
+
+
+
 function displayTemperature(response){
     console.log(response.data.wind.speed);
     let temperatureElement = document.querySelector ("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector ("#city");
-    cityElement.innerHTML = response.data.name;
     let descriptionElement = document.querySelector ("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
     let humidityElement = document.querySelector ("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector ("#wind");
+    let dateElement = document. querySelector ("#date");
+    
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
+    dateElement.innerHTML=formatDate(response.data.dt * 1000);
 }
 
 
@@ -20,4 +43,4 @@ let weatherUrl = `${endpointUrl}&q=New York&appid=${apiKey}&units=${tempetureUni
 
 axios.get(weatherUrl).then(displayTemperature);
 
-let now = new date();
+
