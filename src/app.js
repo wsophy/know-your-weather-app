18db6ebe80413e8548f166745e1945c2bd165cf2
wsophy <96415885+wsophy@users.noreply.github.com>
2022-01-24 13:35:06 +0800

    Add HTML in JS to call days in array

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+
 function formatDate(timestamp){
 let date = new Date(timestamp);
 let hours = date.getHours();
@@ -13,6 +15,33 @@ let day = days[date.getDay()];
 return`${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+let forecastElement = document.querySelector("#forecast");
+
+let days =["Mon","Tue","Wed","Thu","Fri"];
+
+let forecastHTML = `<div class="row">`;
+
+days.forEach(function(day){
+    forecastHTML = forecastHTML + `
+    <div class="col-2">
+      <div class="weather-forecast-day">
+    ${day}
+    </div>
+    <img src="http://openweathermap.org/img/wn/50n@2x.png" alt="" width="42"/>
+    
+    <div class="westher-forecast-temperature">
+    <span class="weather-forecast-temperature-max">18°</span>
+    <span class="weather-forecast-temperature-min">12°</span>
+    </div>
+    </div>
+   `;
+});
+
+forecastHTML=forecastHTML +`</div>`
+forecastElement.innerHTML = forecastHTML;
+
+}
 
 function displayTemperature(response){
     let temperatureElement = document.querySelector ("#temperature");
@@ -23,6 +52,7 @@ function displayTemperature(response){
     let dateElement = document. querySelector ("#date");
     let iconElement = document. querySelector ("#icon");
     
+ 
     celsiusTemperature = response.data.main.temp;
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -72,6 +102,7 @@ temperatureElement.innerHTML = Math.round(celsiusTempeture);
 
 let celsiusTemperature = null;
 
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
