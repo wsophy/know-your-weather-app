71f680a38ea3714999ea1c9c054cee727960d790
wsophy <96415885+wsophy@users.noreply.github.com>
2022-01-30 01:27:50 +0800

    Adding next 6 days forecast and timestamp
    Getting API 6 days weather forecast and corresponding images using index

--- src/app.js
+++ src/app.js
@@ -15,28 +15,36 @@ let day = days[date.getDay()];
 return`${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(response){
-    console.log(response.data.daily);
-let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp){
+let date = new Date(timestamp *1000);
+let day = date.getDay();
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-let days =["Mon","Tue","Wed","Thu","Fri"];
+return days[day];
+}
+
+function displayForecast(response){
+let forecast = response.data.daily;
+
+let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
 
-days.forEach(function(day){
+forecast.forEach(function(forecastDay, index){
+    if (index<6) {
     forecastHTML = forecastHTML + `
     <div class="col-2">
       <div class="weather-forecast-day">
-    ${day}
+    ${formatDay(forecastDay.dt)}
     </div>
-    <img src="http://openweathermap.org/img/wn/50n@2x.png" alt="" width="42"/>
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
     
     <div class="westher-forecast-temperature">
-    <span class="weather-forecast-temperature-max">18°</span>
-    <span class="weather-forecast-temperature-min">12°</span>
+    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
     </div>
     </div>
-   `;
+   `;}
 });
 
 forecastHTML=forecastHTML +`</div>`;
